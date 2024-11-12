c43ffbffc344b2e644fd8a0a293960e3024e7f0f
martindruzbacky <druzbacky.martin@gmail.com>
2024-11-12 18:34:10 +0100

    Added more python scripts

--- python_scripts/annotate_annotsv.py
+++ python_scripts/annotate_annotsv.py
@@ -4,6 +4,6 @@ from helper_functions import safe_run
 
 def annotate_with_annotcv(vcf_input: str):
     annot_output = vcf_input + "_annotSV.tsv"
-    annotsv_command = f"/app/AnnotSV/bin/AnnotSV -SVinputFile {vcf_input} -outputFile {annot_output}"
+    annotsv_command = f"/app/AnnotSV/bin/AnnotSV -SVinputFile {vcf_input} -outputFile {annot_output} -outputDir ."
     safe_run(annotsv_command)
     start_analysis_annotsv(annot_output)
