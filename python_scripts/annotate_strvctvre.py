8d29b0a8f6fd428a4e3ade84c064dfc133f5cb7e
martindruzbacky <druzbacky.martin@gmail.com>
2024-11-13 10:06:28 +0100

    Fixed path to StrVCTVRE

--- python_scripts/annotate_strvctvre.py
+++ python_scripts/annotate_strvctvre.py
@@ -4,7 +4,7 @@ import os
 def annotate_with_strvctvre(vcf_input: str):
     annotated_vcf_output = vcf_input + "_StrVCTVRE.vcf"
 
-    strvctvre_command = f"python StrVCTVRE.py -i {vcf_input} -o {annotated_vcf_output}"
+    strvctvre_command = f"python /app/StrVCTVRE/StrVCTVRE.py -i {vcf_input} -o {annotated_vcf_output}"
     if not os.path.exists(annotated_vcf_output):
         safe_run(strvctvre_command)
     print(f"VCF file annotated using StrVCTVRE. Annotated VCF file: {annotated_vcf_output}")
